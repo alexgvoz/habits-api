2418cdd73db918b06d2671dae4e29a48ed0192be
Alex G <alexandrgvoz@gmail.com>
2020-08-21 17:54:32 -0600

    Added HTML components

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request, render_template, redirect, url_for, abort, flash
 from habit import Habit
-import json
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 habits = [
     Habit("Clean").to_dict(),
@@ -16,40 +16,54 @@ def get_all():
     return render_template("habits.html", habits = habits)
 
 @app.route("/habits/<int:habit_id>", methods = ["GET"])
-def get_one(habitID):
-    for i in range(len(habits)):
-        if habits[i]["id"] == habitID:
-            return habits[i], 200
-        
-    return "", 404
-
-@app.route("/habits", methods = ["POST"])
-def create_habit():
-
-    req = request.get_json()
-    h = Habit(req["title"])
-
-    habits.append(h.to_dict())
-        
-    return h.to_dict(), 201
-
-@app.route("/habits/<int:habit_id>", methods = ["PATCH"])
-def update_habit(habit_id):
-
-    req = request.get_json()
+def get_one(habit_id):
+    habit = None
 
     for i in range(len(habits)):
         if habits[i]["id"] == habit_id:
-            habits[i]["title"] = req["title"]
-            return habits[i], 200
-        
-    return "", 404
+            habit = habits[i]
 
-@app.route("/habits/<int:habit_id>", methods = ["DELETE"])
+    if habit is None:
+        abort(404)
+    
+    return render_template("show_habit.html", habit=habit)
+
+@app.route("/habits", methods = ["POST"])
+def create_habit():
+    if request.form["title"] == "":
+        flash("Error: Title is required!")
+        return redirect(url_for("get_all"))
+
+    h = Habit(request.form["title"])
+
+    habits.append(h.to_dict())
+        
+    return redirect(url_for("get_all"))
+
+@app.route("/habits/edit/<int:habit_id>", methods = ["GET", "POST"])
+def edit_habit(habit_id):
+    habit = None
+
+    for i in range(len(habits)):
+        if habits[i]["id"] == habit_id:
+            habit = habits[i]
+
+    if habit is None:
+        abort(404)
+    
+    if request.method == "GET":
+        return render_template("edit_habit.html", habit=habit)
+
+
+    habit["title"] = request.form["title"]
+
+    return redirect(url_for("get_all"))
+
+@app.route("/habits/delete/<int:habit_id>", methods = ["GET", "POST"])
 def delete_habit(habit_id):
     for i in range(len(habits)):
         if habits[i]["id"] == habit_id:
             habits.pop(i)
-            return jsonify(habits), 200
+            return redirect(url_for("get_all"))
 
     return "", 404
